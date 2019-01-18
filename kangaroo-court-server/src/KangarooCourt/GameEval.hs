2ceb52ba86287decf3196b7a6d461a527267372d
Anthony Super <anthony@noided.media>
2019-01-17 17:16:48 -0700

    Game runner stuff

--- kangaroo-court-server/src/KangarooCourt/GameEval.hs
+++ kangaroo-court-server/src/KangarooCourt/GameEval.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
 module KangarooCourt.GameEval where
-
+    import GHC.Generics
     import KangarooCourt.Command
     import KangarooCourt.ServerError
     import KangarooCourt.ServerState
@@ -12,9 +13,11 @@ module KangarooCourt.GameEval where
     import Control.Monad.Error.Class
     import Data.Coerce
     import Data.Map (Map)
+    import Control.Concurrent.STM.TQueue (TQueue)
+    import Control.Concurrent.STM.TChan (TChan)
 
-    newtype MonadGameEval m a 
-        = MonadGameEval 
+    newtype GameEvalT m a 
+        = GameEvalT
         { evalMG :: StateT ServerState m a 
         } deriving ( Functor
                    , Applicative
@@ -24,5 +27,51 @@ module KangarooCourt.GameEval where
                    )
 
     instance (MonadKangarooCourtPlaying m) =>
-        (MonadKangarooCourtPlaying (MonadGameEval m)) where
+        (MonadKangarooCourtPlaying (GameEvalT m)) where
              getGameEvent = lift getGameEvent
+
+    -- | Two-way communication for games.
+    -- More specifically, this allows individuals to 
+    -- *submit* GameEvents for possible writing, and to
+    -- *read* GameEvents to actually change the game state.
+    -- 
+    -- An example is helpful in demonstrating its use:
+    -- 
+    -- > runPlayerClient :: (MonadSending IO t, MonadRecving IO t)
+    -- >                => t -> GameCommunicator -> IO ()
+    -- > runPlayerClient t c = do
+    -- >     forkIO $ routeInput t (sendQ c)
+    -- >     forever $ routeOutput t (recvC c)
+    -- >
+    -- > routeInput t q = 
+    -- >     readGameMessage t >>= atomically . writeTQueue q
+    -- >
+    -- > routeOutput t c = do
+    -- >     m <- readTChan c
+    -- >     when (userCanSee m) $ sendGameMessage t m
+    --
+    data GameCommunicator
+        = GameCommunicator
+        { sendQ :: TQueue (PlayerId, GameEvent)
+        -- ^ Send an event to the backend.
+        -- The backend server will pop off the events, one
+        -- at a time, and update the game's state and send out
+        -- other events accordingly.
+        , recvC :: TChan (PlayerId, GameEvent) 
+        -- ^ Read the cannonical events from the backend.
+        -- This channel should only ever be used for reading,
+        -- unless you're the game-manager thread.
+        --
+        -- This channel contains a log of all the events
+        -- that have canonically occured inside of the game.
+        -- It is written to by the "game-manager" thread, which
+        -- takes in event requests from the sendQ and forwards
+        -- them to this channel if they are valid.
+        -- If they would place the game in an illegal state,
+        -- the game manager throws them away.
+        -- Reading from this channel step-by-step allows the
+        -- client to fully construct the state of the game.
+        -- As a result, the thread that routes this channel to
+        -- the actual socket __must__ be careful to censor game
+        -- events which the client cannot know about!
+        } deriving (Generic)
---
+++ kangaroo-court-server/src/KangarooCourt/GameRunner.hs
@@ -0,0 +1,204 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ConstraintKinds #-}
+module KangarooCourt.GameRunner where
+
+    import Control.Monad.State.Class
+    import KangarooCourt.ServerState
+    import KangarooCourt.Command
+    import KangarooCourt.Card
+    import Control.Monad.Error.Class
+    import GHC.Generics
+    import Control.Lens
+    import Data.Generics.Product
+    import Data.Generics.Sum
+    import Data.Map.Strict (Map)
+
+    type Checker m =
+        ( MonadState ServerState m
+        , MonadError () m)
+
+
+    ensure err predicate = do
+        s <- get
+        if predicate s then
+            throwError err
+        else pure ()
+
+    ensure' :: (MonadState s m, MonadError () m)
+            => (s -> Bool)
+            -> m () 
+    ensure' = ensure ()
+
+    -- | Commands for the server: sometimes
+    -- the server must be the one to send an event!
+    data ServerCommand
+        = DoNothing
+        -- ^ No further action must be taken
+        | WantJobCard
+        -- ^ We want a job card
+        | EndGame (Map PlayerId Int)
+        -- ^ The game has ended with the given score map
+        deriving (Show, Read, Eq, Ord, Generic)
+    
+    noCommand e = (DoNothing, e)
+    noCommand' e = pure (DoNothing, e)
+
+    callButWait' :: (Checker m) 
+                 => PlayerId 
+                 -> Explanation 
+                 -> m (ServerCommand, GameEvent)
+    callButWait' pid exp = do
+        ensureButWaitable pid 
+        modify $ callButWait pid
+        noCommand' (CallButWait exp)
+
+    acceptRoutine' :: (Checker m)
+                   => PlayerId
+                   -> m (ServerCommand, GameEvent)
+    acceptRoutine' pid = do
+        ensureButWaitable pid
+        modify $ acceptRoutine pid
+        noCommand' AcceptRoutine
+    
+    validateButWait' :: (Checker m)
+                     => PlayerId
+                     -> m (ServerCommand, GameEvent)
+    validateButWait' pid = do
+        ensureButWaitVote pid
+        modify $ validateButWait pid
+        noCommand' ValidateButWait
+
+    rejectButWait' :: (Checker m)
+                   => PlayerId
+                   -> m (ServerCommand, GameEvent)
+    rejectButWait' pid = do
+        ensureButWaitVote pid
+        modify $ rejectButWait pid
+        noCommand' ValidateButWait
+
+    performRoutine' :: (Checker m)
+                    => PlayerId
+                    -> Explanation
+                    -> m (ServerCommand, GameEvent)
+    performRoutine' pid e = do
+        ensure' $ isTurn pid
+        ensure' $ isWaitingPerform
+        modify performRoutine
+        pure (WantJobCard, PerformRoutine e)
+
+    guessAnimal' :: (Checker m)
+                 => PlayerId
+                 -> AnimalCard
+                 -> m (ServerCommand, GameEvent)
+    guessAnimal' pid ac = do
+        ensure' $ isTurn pid
+        ensure' $ isWaitingPerform
+        s <- get
+        case guessAnimal ac pid s of
+            Left e -> withEndGame e ac
+            Right s -> do
+                put s
+                noCommand' $ GuessAnimal ac
+
+    withEndGame e ac = pure (EndGame map, GuessAnimal ac)
+        where
+            map = getField @"playerScore" <$> e
+                
+    -- | Make sure that we're in a state where this player
+    -- either vote to "But wait!" or decide not to
+    ensureButWaitable :: (Checker m)
+                      => PlayerId
+                      -> m ()
+    ensureButWaitable pid = do
+        ensure' isInWaitForButWaits
+        ensure' $ notDecidedButWait pid
+        ensure' $ isNotTurn pid
+
+    -- | Make sure we're in a state where this player can
+    -- vote on if a "but wait!" is valid or not
+    ensureButWaitVote :: (Checker m)
+                      => PlayerId
+                      -> m ()
+    ensureButWaitVote pid = do
+        ensure' isEvaluatingButWait
+        ensure' $ hasNotButWaitVoted pid
+
+    -- | Is our game in the "evaluating a 'but wait!' claim"
+    -- state or not?
+    isEvaluatingButWait :: ServerState -> Bool
+    isEvaluatingButWait
+        = has
+        $ field @"roundState"
+        . field @"roundStage"
+        . _Ctor @"EvaluatingButWait"
+
+    -- | Is our game in the state where players are deciding
+    -- if they want to call a "But wait!"
+    isInWaitForButWaits :: ServerState -> Bool
+    isInWaitForButWaits 
+        = has
+        $ field @"roundState"
+        . field @"roundStage"
+        . _Ctor @"WaitForButWaits"
+    
+    -- | Has the given player not yet decided if they are going
+    -- to call a "But wait!" or not?
+    notDecidedButWait :: PlayerId -> ServerState -> Bool 
+    notDecidedButWait pid
+        = hasn't
+        $ field @"roundState"
+        . field @"roundStage"
+        . _Ctor @"WaitForButWaits"
+        . at pid
+
+    -- | Has the player not yet voted on a "But wait!" or not?
+    hasNotButWaitVoted :: PlayerId -> ServerState -> Bool
+    hasNotButWaitVoted pid
+        = hasn't
+        $ field @"roundState"
+        . field @"roundStage"
+        . _Ctor @"EvaluatingButWait"
+        . _2
+        . at pid 
+
+    -- | Is it not this player's turn?
+    isNotTurn :: PlayerId -> ServerState -> Bool
+    isNotTurn pid
+        = (/= pid)
+        . view
+        ( field @"roundState"
+        . field @"currentPlayer" )
+
+    -- | Is it this player's turn?
+    isTurn :: PlayerId -> ServerState -> Bool
+    isTurn pid
+        = (== pid)
+        . view
+        ( field @"roundState"
+        . field @"currentPlayer" )
+
+
+    -- | Is the game in the "wait for a performance" state?
+    isWaitingPerform :: ServerState -> Bool
+    isWaitingPerform
+        = has
+        $ field @"roundState"
+        . field @"roundStage"
+        . _Ctor @"WaitForPerform"
+
+    runStateCmd (Just p) (PerformRoutine e) =
+        performRoutine' p e
+    runStateCmd (Just p) AcceptRoutine =
+        acceptRoutine' p
+    runStateCmd (Just p) (CallButWait e) =
+        callButWait' p e
+    runStateCmd (Just p) ValidateButWait =
+        validateButWait' p
+    runStateCmd (Just p) RejectButWait =
+        rejectButWait' p
+    runStateCmd (Just p) (GuessAnimal ac) =
+        guessAnimal' p ac
+    runStateCmd _ _ = throwError ()
